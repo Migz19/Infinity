a1f321f4131c575a7742dfb953b3dc82c162180b
One-Nour <nmahmoud@onesolutionc.com>
2023-07-07 13:52:32 +0300

    [ADD] -profile container

--- lib/views/events/events_screen.dart
+++ lib/views/events/events_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:infinity/core/utils/app_assets.dart';
 import 'package:infinity/core/utils/app_color.dart';
 import 'package:infinity/core/utils/media_query.dart';
 import 'package:infinity/models/event/event_model.dart';
@@ -10,7 +11,16 @@ class EventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-
+      appBar: AppBar(
+        title:  Text(
+          "Events",
+          textAlign: TextAlign.start,
+          style: TextStyle(fontWeight: FontWeight.w500,color: AppColor.tertiary),
+        ),
+        toolbarHeight: 0.0,
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
       body: Container(
         color: Colors.white,
         width: context.width,height: context.height,
